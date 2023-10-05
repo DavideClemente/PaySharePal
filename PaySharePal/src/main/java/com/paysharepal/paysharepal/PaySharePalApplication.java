f545283d1f273d65cd43e1b66136a2dacb036c76
Davide Clemente <1190497@isep.ipp.pt>
2023-10-05 15:07:18 +0100

    Initial file creation

---
+++ PaySharePal/src/main/java/com/paysharepal/paysharepal/PaySharePalApplication.java
@@ -0,0 +1,13 @@
+package com.paysharepal.paysharepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaySharePalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaySharePalApplication.class, args);
+	}
+
+}
---
+++ PaySharePal/src/test/java/com/paysharepal/paysharepal/PaySharePalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paysharepal.paysharepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaySharePalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
