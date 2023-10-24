55b200fbe4473ef559b9e73ae4712f2e148cf86d
Davide Clemente <davide.clemente@glintt.com>
2023-10-24 17:59:54 +0100

    Started to add jwt authentication

---
+++ src/main/java/com/paysharepal/paysharepal/config/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.paysharepal.paysharepal.config;
+
+public class SecurityConstants {
+    public static final long EXPRIRATION_TIME = 900_000;
+    public static final String SECRET = "DAVIDESARA123";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL =  "/api/Users";
+}
